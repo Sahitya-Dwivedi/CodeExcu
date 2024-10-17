bdfc19074699141142d0c2abb6b7f1fe7324edef
Sahitya-Dwivedi <sahityadwivedi515@gmail.com>
2024-10-17 12:43:25 +0530

    done console error, warn, info, debug overwrite

--- components/excutor.js
+++ components/excutor.js
@@ -6,19 +6,51 @@ const Excutor = ({ code }) => {
   const handleEval = () => {
     let outputLog = [];
 
-    const OriginalLog = console.log;
+    // saving original console
+    const OriginalConsoleLog = console.log;
+    const OriginalConsoleError = console.error;
+    const OriginalConsoleWarn = console.warn;
+    const OriginalConsoleInfo = console.info;
+    const OriginalConsoleDebug = console.debug;
+
+    // // during designing need to design these also
+    // overwriting console
     console.log = (...args) => {
       outputLog.push(args);
     };
 
+    console.error = (...args) => {
+      outputLog.push(`Error: ${args}`);
+    };
+
+    console.warn = (...args) => {
+      outputLog.push(`Warning: ${args}`);
+    };
+
+    console.info = (...args) => {
+      outputLog.push(`Info: ${args}`);
+    };
+
+    console.debug = (...args) => {
+      outputLog.push(`Debug: ${args}`);
+    };
+
+    // evaluating code
     try {
       code ? eval(code) : eval(localStorage.getItem("code"));
     } catch (error) {
-      outputLog = `Error: ${error.message}`;
+      outputLog.push(`Error: ${error.message}`);
     }
-    console.log = OriginalLog;
+
+    // restoring console
+    console.log = OriginalConsoleLog;
+    console.error = OriginalConsoleError;
+    console.warn = OriginalConsoleWarn;
+    console.info = OriginalConsoleInfo;
+    console.debug = OriginalConsoleDebug;
+
+    // setting value to output log
     setValue(outputLog);
-    console.log(outputLog);
   };
   return (
     <div className="bg-red-900 h-screen w-[50vw]">
@@ -26,9 +58,8 @@ const Excutor = ({ code }) => {
         run
       </button>
       {value.map((val) => {
-        return <div key={Math.random()}>{val}</div>
-      }
-      )}
+        return <div key={Math.random()}>{val}</div>;
+      })}
     </div>
   );
 };
