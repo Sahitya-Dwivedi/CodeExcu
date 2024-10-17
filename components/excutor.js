cd8c2adedf4434bfd696492398c3d41b5e1a8d2f
Sahitya-Dwivedi <sahityadwivedi515@gmail.com>
2024-10-17 11:35:45 +0530

    console log overwrite

--- components/excutor.js
+++ components/excutor.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-const Excutor = ({code}) => {
-  console.log("child", code);
-  
+const Excutor = ({ code }) => {
+  const [value, setValue] = useState("");
+
+  const handleEval = () => {
+    let outputLog = "";
+
+    const OriginalLog = console.log;
+    console.log = (...args) => {
+      outputLog += args;
+    };
+
+    try {
+      code ? eval(code) : eval(localStorage.getItem("code"));
+    } catch (error) {
+      outputLog = `Error: ${error.message}`;
+    }
+    console.log = OriginalLog;
+    setValue(outputLog);
+    console.log(outputLog);
+  };
   return (
-    <div>
-      {code}
+    <div className="bg-red-900 h-screen w-[50vw]">
+      <button onClick={handleEval} className="bg-yellow-800">
+        run
+      </button>
+      <div>{value}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Excutor
+export default Excutor;
