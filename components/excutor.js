72d6add67b63b4a971a09d48b984e2bfc3496324
Sahitya-Dwivedi <sahityadwivedi515@gmail.com>
2024-10-30 12:41:28 +0530

    Refactor console table styling and add support for dynamic table width

--- components/excutor.js
+++ components/excutor.js
@@ -6,155 +6,67 @@ const Excutor = ({ code, toRun, ChangeRun }) => {
 
   const handleEval = useCallback(() => {
     let outputLog = [];
-
-    // Save original console methods
-    const originalConsole = {
-      log: console.log,
-      error: console.error,
-      warn: console.warn,
-      info: console.info,
-      debug: console.debug,
-      table: console.table,
-    };
-
-    // Overwrite console methods
-    const overwriteConsole = () => {
-      console.log = (...args) => {
-        if (Array.isArray(args[0])) {
-          outputLog.push(`[${args.join(" ")}]`);
-        } else if (typeof args[0] === "object") {
-          outputLog.push(JSON.stringify(args[0]));
+    let worker = new Worker(new URL("../public/Worker.js", import.meta.url));
+    let code = localStorage.getItem("code");
+    worker.postMessage(code);
+    worker.onmessage = (e) => {
+      e.data.map((val, i) => {
+        if (typeof val === "object") {
+          const table = (
+            <table key={uuidv4()}>
+              <thead>
+                <tr>
+                  <th>(Index)</th>
+                  {val.headers.map((val, i) => {
+                    return <th key={uuidv4()}>{val}</th>;
+                  })}
+                </tr>
+              </thead>
+              <tbody>
+                {val.rows.map((val, i) => {
+                  return (
+                    <tr key={uuidv4()}>
+                      {Array.isArray(val) &&
+                        val.map((subval) => {
+                          const checkSub = val.findIndex((val) => val === null);
+                          // if (checkSub === -1) {
+                          //   return <td key={uuidv4()}>{subval}</td>;
+                          // } else {
+                          //   if (i== checkSub ) {
+                          //     console.log("null found",checkSub);
+                          //     return (
+                          //       <>
+                          //         <td key={uuidv4()}></td>
+                          //         <td key={uuidv4()}></td>
+                          //         <td key={uuidv4()}>{subval}</td>
+                          //       </>
+                          //     );
+                          //   } else {
+                          //     return (
+                          //       <>
+                          //         <td key={uuidv4()}>{subval}</td>
+                          //       </>
+                          //     );
+                          //   }
+                          // }
+                          return <td key={uuidv4()}>{subval}</td>;
+                        })}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          );
+          outputLog.push(table);
         } else {
-          outputLog.push(args.join(" "));
+          outputLog.push(val);
         }
-      };
-      console.error = (...args) => outputLog.push(`Error: ${args.join(" ")}`);
-      console.warn = (...args) => outputLog.push(`Warning: ${args.join(" ")}`);
-      console.info = (...args) => outputLog.push(`Info: ${args.join(" ")}`);
-      console.debug = (...args) => outputLog.push(`Debug: ${args.join(" ")}`);
-      console.table = (...args) => handleTable(args);
+      });
+      setValue(outputLog);
+      console.log(e.data);
     };
-
-    // Restore original console methods
-    const restoreConsole = () => {
-      console.log = originalConsole.log;
-      console.warn = originalConsole.warn;
-      console.info = originalConsole.info;
-      console.error = originalConsole.error;
-      console.debug = originalConsole.debug;
-      console.table = originalConsole.table;
-    };
-
-    // Handle console.table
-    const handleTable = (args) => {
-      let maxLength = 0;
-      let maxArr = [];
-
-      const is2DArr = (arr) =>
-        Array.isArray(arr[0]) && arr[0].some(Array.isArray);
-      const isEntire2DArray = (arr) =>
-        arr.every((subarray) => Array.isArray(subarray));
-
-      const checkLength = (arr) => {
-        arr[0].forEach((subarr) => {
-          if (subarr.length > maxLength) {
-            maxLength = subarr.length;
-            maxArr = subarr;
-          }
-        });
-      };
-      const createEmptySpaces = (arr) => {
-        let spacesNo = maxLength - arr.length;
-        if (Array.isArray(arr)) {
-          arr.forEach((val) => {
-            if (!val) {
-              throw new SyntaxError("Array should not have empty values");
-            }
-          });
-        }
-        if (Array.isArray(arr)) {
-          for (let i = 0; i < spacesNo; i++) {
-            arr.push("");
-          }
-        }
-      };
-
-      const res = is2DArr(args);
-      let ent2DArrRes = false;
-      if (res) {
-        ent2DArrRes = isEntire2DArray(args[0]);
-      }
-      if (res) {
-        checkLength(args);
-        args[0].forEach((val) => createEmptySpaces(val));
-      }
-
-      if (Array.isArray(args)) {
-        const table = (
-          <table className="ConsoleTable w-1/2 border-2 border-black">
-            <thead>
-              <tr>
-                <th>(Index)</th>
-                {res ? (
-                  maxArr.map((_, i) => <th key={uuidv4()}>{i}</th>)
-                ) : (
-                  <th key={uuidv4()}>Values</th>
-                )}
-                {!ent2DArrRes && res && <th key={uuidv4()}>Values</th>}
-              </tr>
-            </thead>
-            <tbody>
-              {!res
-                ? args[0].map((val, i) => (
-                    <tr key={uuidv4()}>
-                      <td key={uuidv4()}>{i}</td>
-                      <td key={uuidv4()}>{val}</td>
-                    </tr>
-                  ))
-                : args[0].map((val, i) => (
-                    <tr key={uuidv4()}>
-                      <td key={uuidv4()}>{i}</td>
-                      {Array.isArray(val)
-                        ? val.map((subval) => <td key={uuidv4()}>{subval}</td>)
-                        : maxArr.map((_, j) =>
-                            j === maxArr.length - 1 ? (
-                              <React.Fragment key={uuidv4()}>
-                                <td key={uuidv4()}></td>
-                                <td key={uuidv4()}>{val}</td>
-                              </React.Fragment>
-                            ) : (
-                              <td key={uuidv4()}>{""}</td>
-                            )
-                          )}
-                    </tr>
-                  ))}
-            </tbody>
-          </table>
-        );
-        outputLog.push(table);
-      }
-    };
-
-    // Evaluate code
-    const evaluateCode = () => {
-      try {
-        code ? eval(code) : eval(localStorage.getItem("code"));
-      } catch (error) {
-        console.error(error);
-        originalConsole.error(error);
-        outputLog.push(`ErrorMsg: ${error.message}`);
-      }
-    };
-
-    // Execute evaluation
-    overwriteConsole();
-    evaluateCode();
-    restoreConsole();
-
-    // Set output log
-    setValue(outputLog);
   }, [code]);
-  
+
   useEffect(() => {
     if (toRun) {
       handleEval();
---
+++ public/Worker.js
@@ -0,0 +1,186 @@
+self.onmessage = (e) => {
+  let outputLog = [];
+
+  // Save original console methods
+  const originalConsole = {
+    log: console.log,
+    error: console.error,
+    warn: console.warn,
+    info: console.info,
+    debug: console.debug,
+    table: console.table,
+  };
+
+  // Overwrite console methods
+  const overwriteConsole = () => {
+    // console.log = (...args) => {
+    //   if (Array.isArray(args[0])) {
+    //     outputLog.push(`[${args.join(" ")}]`);
+    //   } else if (typeof args[0] === "object") {
+    //     outputLog.push(JSON.stringify(args[0]));
+    //   } else {
+    //     outputLog.push(args.join(" "));
+    //   }
+    // };
+    console.error = (...args) => outputLog.push(`Error: ${args.join(" ")}`);
+    console.warn = (...args) => outputLog.push(`Warning: ${args.join(" ")}`);
+    console.info = (...args) => outputLog.push(`Info: ${args.join(" ")}`);
+    console.debug = (...args) => outputLog.push(`Debug: ${args.join(" ")}`);
+    console.table = (...args) => handleTable(args);
+  };
+
+  // Restore original console methods
+  const restoreConsole = () => {
+    console.log = originalConsole.log;
+    console.warn = originalConsole.warn;
+    console.info = originalConsole.info;
+    console.error = originalConsole.error;
+    console.debug = originalConsole.debug;
+    console.table = originalConsole.table;
+  };
+
+  // Handle console.table
+  const handleTable = (args) => {
+    let maxLength = 0;
+    let maxArr = [];
+
+    const is2DArr = (arr) =>
+      Array.isArray(arr[0]) && arr[0].some(Array.isArray);
+    const isEntire2DArray = (arr) =>
+      arr.every((subarray) => Array.isArray(subarray));
+
+    const checkLength = (arr) => {
+      arr[0].forEach((subarr) => {
+        if (subarr.length > maxLength) {
+          maxLength = subarr.length;
+          maxArr = subarr;
+        }
+      });
+    };
+
+    const res = is2DArr(args);
+    let ent2DArrRes = false;
+    let NewArr = [];
+    if (res) {
+      ent2DArrRes = isEntire2DArray(args[0]);
+      checkLength(args);
+      NewArr = args[0].map((val) => {
+        if (Array.isArray(val)) {
+          let spacesNo = maxLength - val.length;
+          for (let i = 0; i < spacesNo; i++) {
+            val.push("");
+          }
+          
+          return val;
+        } else {
+          let spacesNo = maxLength;
+          let genSpaces = [];
+          for (let i = 0; i < spacesNo; i++) {
+            genSpaces.push("");
+          }
+          return [...genSpaces, val];
+        }
+      });
+
+      console.log(NewArr);
+    }
+    headers = () => {
+      if (ent2DArrRes) {
+        return maxArr.map((_, i) => i);
+      } else if (res) {
+        let head = maxArr.map((_, i) => i);
+        return [...head, "Values"];
+      } else {
+        return ["Values"];
+      }
+    };
+    console.log(NewArr);
+    if (Array.isArray(args[0])) {
+      const tableData = {
+        headers: headers(),
+        rows: !res
+          ? args[0].map((val, i) => [i, val])
+          : NewArr.map((val, i) => {
+              if (Array.isArray(val)) {
+                return [i, ...val];
+              } else {
+                console.log(maxArr);
+                return maxArr.map((_, j) => {
+                  if (j === maxArr.length - 1) {
+                    return val;
+                  } else {
+                    if (j === 0) {
+                      return i;
+                    } else {
+                      return null;
+                    }
+                  }
+                });
+              }
+            }),
+      };
+      //   const table = (
+      //     <table className="ConsoleTable w-1/2 border-2 border-black">
+      //       <thead>
+      //         <tr>
+      //           <th>(Index)</th>
+      //           {res ? (
+      //             maxArr.map((_, i) => <th key={uuidv4()}>{i}</th>)
+      //           ) : (
+      //             <th key={uuidv4()}>Values</th>
+      //           )}
+      //           {!ent2DArrRes && res && <th key={uuidv4()}>Values</th>}
+      //         </tr>
+      //       </thead>
+      //       <tbody>
+      //         {!res
+      //           ? args[0].map((val, i) => (
+      //               <tr key={uuidv4()}>
+      //                 <td key={uuidv4()}>{i}</td>
+      //                 <td key={uuidv4()}>{val}</td>
+      //               </tr>
+      //             ))
+      //           : args[0].map((val, i) => (
+      //               <tr key={uuidv4()}>
+      //                 <td key={uuidv4()}>{i}</td>
+      //                 {Array.isArray(val)
+      //                   ? val.map((subval) => <td key={uuidv4()}>{subval}</td>)
+      //                   : maxArr.map((_, j) =>
+      //                       j === maxArr.length - 1 ? (
+      //                         <div key={uuidv4()}>
+      //                           <td key={uuidv4()}></td>
+      //                           <td key={uuidv4()}>{val}</td>
+      //                         </div>
+      //                       ) : (
+      //                         <td key={uuidv4()}>{""}</td>
+      //                       )
+      //                     )}
+      //               </tr>
+      //             ))}
+      //       </tbody>
+      //     </table>
+      //   );
+      outputLog.push(tableData);
+    }
+  };
+
+  // Evaluate code
+  const evaluateCode = () => {
+    try {
+      e.data ? eval(e.data) : eval(localStorage.getItem("code"));
+    } catch (error) {
+      console.error(error);
+      originalConsole.error(error);
+      outputLog.push(`ErrorMsg: ${error.message}`);
+    }
+  };
+
+  // Execute evaluation
+  overwriteConsole();
+  evaluateCode();
+  restoreConsole();
+
+  // Set output log
+  //   setValue(outputLog);
+  self.postMessage(outputLog);
+};
