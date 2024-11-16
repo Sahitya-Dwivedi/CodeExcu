773f87adb1606526e966246c9e3b74aad77de5ce
Sahitya-Dwivedi <sahityadwivedi515@gmail.com>
2024-11-16 13:53:57 +0530

    Enhance Navbar component with improved styling and layout for the run button

--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { FaPlay } from "react-icons/fa";
 
 const Navbar = ({ letRun }) => {
   return (
-    <nav className="w-screen h-[10vh] bg-blue-800">
-      <button onClick={() => letRun(true)}>run</button>
+    <nav className="w-screen h-[10vh] border-2 border-b-green-500 border-black rounded-lg relative">
+      <h1 className="text-3xl font-bold font-serif m-2 p-2">CodeExcu</h1>
+      <button
+        onClick={() => letRun(true)}
+        className="bg-yellow-500 absolute right-0 bottom-0 my-2 mx-4 px-2 text-lg rounded-md"
+      >
+        {/* <FaPlay /> */}
+        run
+      </button>
     </nav>
   );
 };
