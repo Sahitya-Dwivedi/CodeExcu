082ea7419c1e931ff6b6580f9132647187bcdd64
Sahitya-Dwivedi <sahityadwivedi515@gmail.com>
2025-01-25 08:21:14 +0530

    Add alert in Home component to indicate ongoing development progress

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CodeEditor from "@/components/editor";
 import Navbar from "@/components/navbar";
 import Excutor from "@/components/excutor";
@@ -7,7 +7,10 @@ import Excutor from "@/components/excutor";
 const Home = () => {
   const [code, setCode] = useState("");
   const [run, setRun] = useState(false);
-
+  useEffect(() => {
+    alert("The making is in progress. current functioning can be incomplete also");
+  }, [])
+  
   return (
     <div className="flex flex-col justify-end items-end bg-gradient-to-r from-[#0000ff] to-[#ee005f]  h-screen">
       <Navbar letRun={setRun} />
